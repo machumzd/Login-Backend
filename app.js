1d65a001408faa17642a64c4478fde3a39c6539b
machumzd <www.machuzz123@gmail.com>
2022-12-11 23:43:40 +0530

    backend completed,metamask added as admin,validation completed

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const session=require('express-session');
+app.use(session({
+  secret:"key",
+  cookie:{maxAge:600000}
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,57 @@
-const { dir } = require('console');
-var express = require('express');
-const { dirname } = require('path');
+const { dir } = require("console");
+var express = require("express");
+const { dirname } = require("path");
 var router = express.Router();
-var path=require("path");
+var path = require("path");
+const { response } = require("../app");
 
-/* GET home page. */ 
-router.get('/', function(req, res, next) {
-  res.render('index',{title:"person"})
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  console.log("index get", req.session.user);
+  if (req.session.user) {
+    res.redirect("home");
+  } else {
+    res.render("index", { title: "person" });
+  }
 });
 
-
-router.post('/submit',function(req,res){
-  var pass=req.body.password;
-  res.render('index',{password:pass})
-})
-router.get('/signup', function(req, res, next) {
-  res.render('signup',{title:"signup"})
+router.post("/submit", function (req, res) {
+  var uName = req.body.uName;
+  var pass = req.body.password;
+  var error;
+  if (uName == "Admin" && pass == 7994) {
+    req.session.user = true;
+    req.session.userdata = { pass, uName };
+    res.redirect("/home");
+  } else if (uName == "" && pass == "") {
+    error = "enter the credentials";
+    res.render("index", { error });
+  } else if (uName == "" || pass == "") {
+    if (pass == "") {
+      error = "enter the password";
+      res.render("index", { error });
+    } else {
+      error = "enter the username";
+      res.render("index", { error });
+    }
+  } else {
+    error = "Wrong credentials ";
+    res.render("index", { error });
+  }
+});
+router.get("/home", function (req, res) {
+  if (req.session.user) {
+    username = req.session.userdata.uName;
+    userpass = req.session.userdata.pass;
+    res.render("main", { username, userpass });
+  } else {
+    res.redirect("/");
+  }
 });
 
-
-
+router.get("/logout", function (req, res) {
+  req.session.user = false;
+  res.render("index");
+});
 
 module.exports = router;
