f46a71d9b7843aa6b385e468f147d7a6191bff18
machumzd <www.machuzz123@gmail.com>
2022-12-12 13:38:58 +0530

    backarrow issue solved,session handles

--- routes/index.js
+++ routes/index.js
@@ -1,57 +1,52 @@
 const { dir } = require("console");
 var express = require("express");
+const session = require("express-session");
 const { dirname } = require("path");
 var router = express.Router();
 var path = require("path");
 const { response } = require("../app");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  console.log("index get", req.session.user);
-  if (req.session.user) {
-    res.redirect("home");
-  } else {
-    res.render("index", { title: "person" });
-  }
-});
 
-router.post("/submit", function (req, res) {
-  var uName = req.body.uName;
-  var pass = req.body.password;
-  var error;
-  if (uName == "Admin" && pass == 7994) {
-    req.session.user = true;
-    req.session.userdata = { pass, uName };
-    res.redirect("/home");
-  } else if (uName == "" && pass == "") {
-    error = "enter the credentials";
-    res.render("index", { error });
-  } else if (uName == "" || pass == "") {
-    if (pass == "") {
-      error = "enter the password";
-      res.render("index", { error });
-    } else {
-      error = "enter the username";
-      res.render("index", { error });
-    }
-  } else {
-    error = "Wrong credentials ";
-    res.render("index", { error });
-  }
-});
-router.get("/home", function (req, res) {
+
+router.get("/", function (req, res, next) {
+  let msg
   if (req.session.user) {
     username = req.session.userdata.uName;
     userpass = req.session.userdata.pass;
     res.render("main", { username, userpass });
   } else {
-    res.redirect("/");
+    if (req.session.msg) {
+      msg='Wrong Credentials'
+    }else{
+      msg=''
+    }
+
+    res.render("index", { error: msg });
   }
 });
 
+router.post("/submit", function (req, res) {
+  if (!req.session.user) {
+    var uName = req.body.uName;
+  var pass = req.body.password;
+  if (uName == "Admin" && pass == 7994) {
+    req.session.user = true;
+    req.session.userdata = { pass, uName };
+    req.session.msg=false
+  }else {
+    req.session.msg=true  
+  }
+  }
+  res.redirect("/");
+});
+
+
+
 router.get("/logout", function (req, res) {
-  req.session.user = false;
-  res.render("index");
+req.session.user = false;
+req.session.destroy();
+  res.redirect("/");
 });
 
 module.exports = router;
